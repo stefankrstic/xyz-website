d02dc08d2fa8c42f7467c80489082b01cd464396
stefankrstic <stefan.k.krstic@gmail.com>
2024-04-23 21:20:04 +0200

    Create about

---
+++ src/components/About.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <>
+            <div className="md:px-14 p-4 max-w-s max-auto space-y-10">
+                <div className="flex flex-col md:flex-row justify-between items-center gap-8">
+                    <div className="md:w-1/2">
+                        <img src="https://ecocix.com/static/media/GirlReading.36b735af4c8b051d12099388f0f8b6b8.svg"></img>
+                    </div>
+                    <div className="md:w-2/5">
+                        <h2 className="md:text-5xl text-3xl font-bold text-primary mb-5 leading-normal">
+                            We have been improving our product <span className="text-secondary">for many years.</span>
+                        </h2>
+                        <p className="text-tartiary text-lg mb-7">
+                            A good example of a paragraph contains a topic conclusion. There are many diffrent kinds of
+                            animals that live in forest.
+                        </p>
+                        <button className="py-2 px-8 bg-secondary font-semibold text-white rounded hover:bg-primary transition-all duration-300">
+                            Get Started
+                        </button>
+                    </div>
+                </div>
+                <div className="flex flex-col md:flex-row-reverse justify-between items-center gap-8">
+                    <div className="md:w-1/2">
+                        <img src="https://i0.wp.com/kartaca.com/wp-content/uploads/2023/03/Untitled-design-2.png?w=1180&ssl=1"></img>
+                    </div>
+                    <div className="md:w-2/5">
+                        <h2 className="md:text-5xl text-3xl font-bold text-primary mb-5 leading-normal">
+                            You can Pracrice at any <span className="text-secondary">time convinent for you.</span>
+                        </h2>
+                        <p className="text-tartiary text-lg mb-7">
+                            A good example of a paragraph contains a topic conclusion. There are many diffrent kinds of
+                            animals that live in forest.
+                        </p>
+                        <button className="py-2 px-8 bg-secondary font-semibold text-white rounded hover:bg-primary transition-all duration-300">
+                            Get Started
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Home from "@/components/Home";
 import Features from "@/components/Features";
+import About from "@/components/About";
 
 export default function App() {
     return (
@@ -8,6 +9,7 @@ export default function App() {
             <Navbar />
             <Home />
             <Features />
+            <About />
         </main>
     );
 }
