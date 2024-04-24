ae9314cd36a8994b70b771ed850e9ca597c20bc4
stefankrstic <stefan.k.krstic@gmail.com>
2024-04-24 17:11:30 +0200

    Create Pricing

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Home from "@/components/Home";
 import Features from "@/components/Features";
 import About from "@/components/About";
+import Pricing from "@/components/Pricing";
 
 export default function App() {
     return (
@@ -10,6 +11,7 @@ export default function App() {
             <Home />
             <Features />
             <About />
+            <Pricing />
         </main>
     );
 }
